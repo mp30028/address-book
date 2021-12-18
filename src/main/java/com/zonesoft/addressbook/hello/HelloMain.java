e1acb1637f728b006aa6240773b24addb18c31ec
mp30028 <mp30028@gmail.com>
2021-12-18 06:09:35 +0000

    added thymeleaf support

---
+++ src/main/java/com/zonesoft/addressbook/hello/HelloMain.java
@@ -0,0 +1,22 @@
+package com.zonesoft.addressbook.hello;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import java.time.LocalDateTime;
+public class HelloMain {
+    public static void main(String[] args) {
+        TemplateEngine templateEngine = new TemplateEngine();
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        resolver.setPrefix("/templates/");
+        resolver.setSuffix(".html");
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setTemplateMode(TemplateMode.HTML); // HTML5 option was deprecated in 3.0.0
+        templateEngine.setTemplateResolver(resolver);
+        Context ct = new Context();
+        ct.setVariable("name", "foo");
+        ct.setVariable("date", LocalDateTime.now().toString());
+        System.out.println(templateEngine.process("greeting.html", ct));
+    }
+}
